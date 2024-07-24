655657b5eb1274766e8ef40ed8618f6ab51150ec
chenhao <a01204000@gmail.com>
2024-07-24 16:11:38 +0800

    商品功能- 實作修改商品

--- springboot-mall/src/main/java/com/chenhao/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/chenhao/springbootmall/dao/ProductDao.java
@@ -6,4 +6,5 @@ import com.chenhao.springbootmall.model.Product;
 public interface ProductDao {
     Product getProductById(Integer id);
     Integer createProduct(ProductRequest productRequest);
+    void updateProduct(Integer productId,ProductRequest productRequest);
 }
