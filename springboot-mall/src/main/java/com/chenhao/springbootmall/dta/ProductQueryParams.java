609f2120e01197386129a24dff112a2c1dc554aa
chenhao <a01204000@gmail.com>
2024-07-30 10:25:38 +0800

    商品功能- 實作查詢商品列表(排序)

--- springboot-mall/src/main/java/com/chenhao/springbootmall/dta/ProductQueryParams.java
+++ springboot-mall/src/main/java/com/chenhao/springbootmall/dta/ProductQueryParams.java
@@ -5,6 +5,24 @@ import com.chenhao.springbootmall.constant.ProductCategory;
 public class ProductQueryParams {
     private ProductCategory category;
     private String search;
+    private String orderBy;
+    private String sort;
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+
+    public String getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
+    }
 
     public ProductCategory getCategory() {
         return category;
